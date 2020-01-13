e1e810e882c6f0ccb056d4e2c5b1a439e0e4af15
u0u0 <hello@tyrantek.com>
2020-01-13 11:31:17 +0800

    py脚本过渡到python 3.x

--- tools/CreateProject.py
+++ tools/CreateProject.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 """
 NAME
@@ -58,42 +58,40 @@ def initDict(names, isLandscape):
 
 def stringReplace(s):
     for key in replacementDict:
-        s = re.sub(key, replacementDict[key], s)
+        s = s.replace(key, replacementDict[key])
     return s
 
 def checkPackageName(packageName):
-    print "====> Checking packageName validity."
+    print("====> Checking packageName validity.")
     packageName = packageName.lower()
     arr =  packageName.split(".")
     if len(arr) != 3:
-        print "Error: packageName must be composed of three sections connected by points"
+        print("Error: packageName must be composed of three sections connected by points")
         sys.exit(-2)
     for item in arr:
         if not checkStringValid(item):
-            print "Error: %s must be composed of English characters(a~z) and number(0~9)" %(item)
+            print("Error: %s must be composed of English characters(a~z) and number(0~9)" %(item))
             sys.exit(-2)
-    print "====> PackageName is valid."
+    print("====> PackageName is valid.")
     return arr
 
 def createRootPath(outputDir):
     if os.path.exists(outputDir):
-        print "Error: %s is already exist" %(outputDir)
+        print("Error: %s is already exist" %(outputDir))
         sys.exit(-2)
     os.mkdir(outputDir)
     if not os.path.exists(outputDir):
-        print "Error: create %s fail" %(outputDir)
+        print("Error: create %s fail" %(outputDir))
         sys.exit(-2)
 
 def copyFile(src, dest):
-    buff = ""
     inFp = open(src, 'rb')
-    for line in inFp.readlines(): 
-        line = stringReplace(line)
-        buff += line
-    inFp.close()
-
     outFp = open(dest, 'wb')
-    outFp.write(buff)
+    for line in inFp.readlines():
+        # ISO8859-1 is safe for any bytes <=> str
+        line = stringReplace(line.decode("ISO8859-1"))
+        outFp.write(line.encode("ISO8859-1"))
+    inFp.close()
     outFp.close()
 
 def copyDir(src, dest):
@@ -116,7 +114,7 @@ def replaceFileContent(filePath, replaceItems):
     with open(filePath) as f:
         fileContent = f.read()
 
-    for old, new in replaceItems.items():
+    for old, new in list(replaceItems.items()):
         fileContent = fileContent.replace(old, new)
 
     with open(filePath, "w") as f:
@@ -131,14 +129,14 @@ def createProject(packageName, outputDir, isLandscape, needCopyCocos2d):
     templateRoot = joinDir(engineRoot, "templates", "lua-template-default")
     outputDir = joinDir(outputDir, lastName)
 
-    print "====> Copying template."
+    print("====> Copying template.")
     createRootPath(outputDir)
     copyDir(templateRoot, outputDir)
-    print "====> Copying cocos."
+    print("====> Copying cocos.")
     shutil.copytree(joinDir(engineRoot, "cocos", "scripting", "lua-bindings", "script"), joinDir(outputDir, "src", "cocos"))
 
     if needCopyCocos2d:
-        print "====> Copying cocos sources."
+        print("====> Copying cocos sources.")
         os.mkdir(joinDir(outputDir, "frameworks", "cocos2d-x"))
         for dirName in ["cmake", "cocos", "extensions", "external"]:
             shutil.copytree(joinDir(engineRoot, dirName), joinDir(outputDir, "frameworks", "cocos2d-x", dirName))
@@ -146,7 +144,7 @@ def createProject(packageName, outputDir, isLandscape, needCopyCocos2d):
             copyFile(joinDir(engineRoot, fileName), joinDir(outputDir, "frameworks", "cocos2d-x", fileName))
         copyFile(joinDir(engineRoot, "cocos", "scripting", "lua-bindings", "manual", "lua_module_register.h"), joinDir(outputDir, "frameworks", "runtime-src", "Classes", "lua_module_register.h"))
         shutil.rmtree(joinDir(outputDir, "frameworks", "cocos2d-x", "cocos", "scripting", "lua-bindings", "script")) # remove double files
-    print "====> Done."
+    print("====> Done.")
 
 if __name__ == "__main__":
     # ===== parse args =====
@@ -154,7 +152,7 @@ if __name__ == "__main__":
         opts, args = getopt.getopt(sys.argv[1:], "hp:o:l")
     except getopt.GetoptError:
         # print help information and exit:
-        print __doc__
+        print(__doc__)
         sys.exit(-2)
 
     packageName = ""
@@ -164,7 +162,7 @@ if __name__ == "__main__":
     for o, a in opts:
         if o == "-h":
             # print help information and exit:
-            print __doc__
+            print(__doc__)
             sys.exit(0)
         if o == "-p":
             packageName = a
@@ -174,12 +172,12 @@ if __name__ == "__main__":
             isLandscape = True
 
     if len(packageName) == 0:
-        print "Error: use -p xxx.xxx.xxx to set package name"
+        print("Error: use -p xxx.xxx.xxx to set package name")
         sys.exit(-2)
     if len(outputDir) == 0:
-        print "Error: use -o /path/path/ to set output dir"
+        print("Error: use -o /path/path/ to set output dir")
         sys.exit(-2)
 
     # info printing
-    print "== engineRoot: %s" %(engineRoot)
+    print("== engineRoot: %s" %(engineRoot))
     createProject(packageName, outputDir, isLandscape, needCopyCocos2d)
